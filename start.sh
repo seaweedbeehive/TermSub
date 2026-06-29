06cb4b3596ab3fdf53934ed27cf25776eefe2a97
seaweedbeehive <sbehb91@gmail.com>
2026-06-29 22:33:51 +0200

    fix: run all alembic branch heads on deploy

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Run database migrations first
-alembic upgrade head
+alembic upgrade heads
 
 # Start Celery worker in background
 celery -A app.core.celery_app worker --loglevel=info --concurrency=2 &
